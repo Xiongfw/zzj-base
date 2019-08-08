634cb3ad2d9e896f408ac9dc2865b06eaf6c8b23
Xiongfw <xiongfw@qq.com>
2019-08-08 14:42:44 +0800

    fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,15 +14,10 @@ let ORG_CONFIG_URL = "http://192.168.0.119:8014/api/"
 
 // 开发环境的常量地址
 if (process.env.NODE_ENV == "development") {
-  // WEB_SOCKET_URL = "ws://192.168.0.119:8085/websocket/"
-  // Unified_Payment_URL = "http://192.168.0.119:8888/api/"
-  // Service_Window_URL = "http://192.168.0.119:8088/api/"
-  // ORG_CONFIG_URL = "http://192.168.0.119:8085/api/"
 }
 
 export default {
   Unified_Payment_URL,
   Service_Window_URL,
-  WEB_SOCKET_URL,
   ORG_CONFIG_URL,
 }
