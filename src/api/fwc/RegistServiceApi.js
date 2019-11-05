c5a4a44162b696a223bbbca18c468e79ea6ea473
Xiongfw <xiongfw@qq.com>
2019-11-05 14:15:46 +0800

    0

--- src/api/fwc/RegistServiceApi.js
+++ src/api/fwc/RegistServiceApi.js
@@ -17,6 +17,12 @@ export default {
     return axios.get(baseUrl + 'IF_Get_Regist_Doc_Resource', { params, options: { log: false, ...options } })
   },
   /**
+   * 23.3、获取医生挂号号源
+   */
+  IF_Get_Regist_Doc_Resource_DataSource(params, options = {}) {
+    return axios.get(baseUrl + 'IF_Get_Regist_Doc_Resource_DataSource', { params, options: { log: false, ...options } })
+  },
+  /**
    * 23.6、确认挂号
    * 先调服务窗 23.6、确认挂号 IF_Confirm_Regist
    * 根据 HospitalTradeApplyInfo 键取OutTradeNo、TotalAmount值传入贝尔曼统一支付平台处理HIS支付相关订单 PAY_Hospital_Trade接口
