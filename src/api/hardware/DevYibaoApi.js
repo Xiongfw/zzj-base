2aba14420a2656def03effef0f99dec0cfbc1ea5
Xiongfw <xiongfw@qq.com>
2019-11-26 16:46:32 +0800

    update

--- src/api/hardware/DevYibaoApi.js
+++ src/api/hardware/DevYibaoApi.js
@@ -4,7 +4,7 @@ import Constants from '@/constants/index'
 const defaultOptions = { log: false, loading: false, alert: false, hardware: true }
 
 export default {
-  baseUrl: Constants.ext_device_url + 'yibao/',
+  baseUrl: Constants.yibao_url + 'yibao/',
   /* 保障卡基本信息读取 */
   ReadBaseInfo(params, options = {}) {
     return axios({ method: 'get', url: this.baseUrl + 'ReadBaseInfo', params, options: { ...defaultOptions, ...options } });
