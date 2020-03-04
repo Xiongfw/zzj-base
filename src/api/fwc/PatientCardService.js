986247d262bdbfa8e3f0128fb21bd9f25013fb2a
xiongfw <xiongfw@qq.com>
2020-03-04 09:17:22 +0800

    调整接口

--- src/api/fwc/PatientCardService.js
+++ src/api/fwc/PatientCardService.js
@@ -7,43 +7,49 @@ export default {
   /**
    * 1.1、创建就诊卡
    */
-  IF_Create_MedicalCard(params, options = {}) {
-    return axios.post(baseUrl + 'IF_Create_MedicalCard', params, { options })
+  IF_Create_MedicalCard(data, options = {}) {
+    return axios({method: 'post', url: baseUrl + 'IF_Create_MedicalCard', data, options })
   },
   /**
    * 1.2、登记就诊卡
    */
-  IF_Regist_MedicalCard(params, options = {}) {
-    return axios.post(baseUrl + 'IF_Regist_MedicalCard', params, { options })
+  IF_Regist_MedicalCard(data, options = {}) {
+    return axios({method: 'post', url: baseUrl + 'IF_Regist_MedicalCard', data, options })
   },
   /**
    * 1.3、注销就诊卡
    */
-  IF_Cancel_MedicalCard(params, options = {}) {
-    return axios.post(baseUrl + 'IF_Cancel_MedicalCard', params, { options })
+  IF_Cancel_MedicalCard(data, options = {}) {
+    return axios({method: 'post', url: baseUrl + 'IF_Cancel_MedicalCard', data, options })
   },
   /**
    * 1.4、清除就诊卡
    */
   IF_Clear_MedicalCard(params, options = {}) {
-    return axios.get(baseUrl + 'IF_Clear_MedicalCard', { params, options })
+    return axios({method: 'get', url: baseUrl + 'IF_Clear_MedicalCard', params, options })
   },
   /**
    * 1.5、获取医院已有卡列表
    */
   IF_Get_Hospital_MedicalCard_List(params, options = {}) {
-    return axios.get(baseUrl + 'IF_Get_Hospital_MedicalCard_List', { params, options })
+    return axios({method: 'get', url: baseUrl + 'IF_Get_Hospital_MedicalCard_List', params, options })
   },
   /**
    * 1.8、刷新就诊卡
    */
   IF_Refresh_MedicalCard(params, options = {}) {
-    return axios.get(baseUrl + 'IF_Refresh_MedicalCard', { params, options })
+    return axios({method: 'get', url: baseUrl + 'IF_Refresh_MedicalCard', params, options })
   },
   /**
    * 1.9、获取绑卡人信息
    */
   IF_Get_BindCardPerson_Info(params, options = {}) {
-    return axios.get(baseUrl + 'IF_Get_BindCardPerson_Info', { params, options })
+    return axios({method: 'get', url: baseUrl + 'IF_Get_BindCardPerson_Info', params, options })
+  },
+  /**
+   * 1.9、获取绑卡人信息（新）
+   */
+  IF_Get_Person_Info(params, options = {}) {
+    return axios({method: 'get', url: baseUrl + 'IF_Get_Person_Info', params, options })
   },
 }
